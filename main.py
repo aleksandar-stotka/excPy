70e6c9ca8dc68795c488675ff7511973bfb6a987
aleksandar-stotka <aangelevski6@gmail.com>
2024-10-22 13:01:01 +0200

    check_word

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
+import random
+words = ['tabla', "zanaet", "kuka"]
 
+
+random_words = random.choice(words)
+
+print(random_words)
+
+placeholder = ""
+
+guess = input("guess words ").lower()
+
+if random_words == guess:
+    print('win')
+else: print("no")    
 
 
