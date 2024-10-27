1ae289b8e5a5eccacd5f2503d6b107b2f725cea1
aleksandar-stotka <aangelevski6@gmail.com>
2024-10-27 11:02:58 +0100

    ENETER-ODD

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
-calculate_of_units = 24 
-name_of_units = "hours"
+def main():
+    x = int(input("write your number "))
+    if x % 2 == 0:
+        print("even")
+    else:
+        print("odd")    
+        
+        
+main()        
 
 
+def is_even(n):
+    if n % 2 == 0:
+        return True
+    else:
+        return False
+    
+user_out = is_even(2)
 
-def days_of_units(number_of_days):
-      return f"{number_of_days} need {number_of_days * calculate_of_units} {name_of_units}"
-
-
-user_input = input ("hey user enter number of days and will be convert to  hours \n")
-user_input_number = int(user_input)
-
-
-calculate_value = days_of_units(user_input_number)
-
-print(calculate_value)
+print(user_out)
