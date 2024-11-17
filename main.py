0bb6db9ba28a86f25b0a10d4984c9eb9e271270d
aleksandar-stotka <aangelevski6@gmail.com>
2024-11-17 11:46:13 +0100

    practice-py-hag

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import random
-'''
+
+"""
 def main():
     x = int(input("write your number "))
     if x % 2 == 0:
@@ -23,24 +24,24 @@ fine_score = is_even(user_out)
 
 print(fine_score)
 
-'''
+"""
 
-words = ['tapan','kuka',"majka"]
+words = ["tapan", "kuka", "majka"]
 
 random_word = random.choice(words)
-print(random_word)
 
+
+print(random_word)
+          
 display = "_"
 display_letter = []
+
+guess_letter = input("guess the letter ")
+
+
 for letter in random_word:
-      letter = display
-      print(letter)
-      
-
-        
-    
-
-
-
-
-
+    letter == display
+    if letter == guess_letter:
+        display_letter.append(letter)
+     
+    print(display_letter)
