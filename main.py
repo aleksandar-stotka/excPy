a403b95d976125c8db2f8d63102c6b7b01fa2a8d
aleksandar-stotka <aangelevski6@gmail.com>
2024-12-24 13:35:15 +0100

    done-score

--- main.py
+++ main.py
@@ -1,46 +1,33 @@
-import random
-
-"""
-def main():
-    x = int(input("write your number "))
-    if x % 2 == 0:
-        print("even")
-    else:
-        print("odd")    
-        
-        
-main()        
+student_scores = {
+    'Harry': 88,
+    'Ron': 78,
+    'Hermione': 95,
+    'Draco': 75,
+    'Neville': 60
+}
+student_name = input('student name ')
+student_score = int(input("score is "))
 
 
-def is_even(n):
-    if n % 2 == 0:
-        return True
-    else:
-        return False
+
+student_grades = {}
+for student, score in student_scores.items():
+    if 91 <= score <= 100:
+     student_grades[student_name,student_score] = "Outstanding"
+    if 81 <= score <= 90:
+         student_grades[(f"{student_name} is {student_score}")]= "Exceeds Expectations"
+    if 71 <= score <= 80:
+         student_grades[(f"{student_name} is {student_score}")]= "Acceptable"
+    elif score <= 70:
+         student_grades = "fail"
     
-user_out = int(input("choose number "))
-
-fine_score = is_even(user_out)
-
-print(fine_score)
-
-"""
-
-words = ["tapan", "kuka", "majka"]
-
-random_word = random.choice(words)
-
-
-print(random_word)
-          
-display = ""
-
-guess_letter = input("guess the letter ").lower()
-
-for letter in random_word:
-    if letter == guess_letter:
-        display += letter
-    else:
-        display += "_"
+print(student_grades)    
         
-print(display)      
+  
+       
+
+
+    
+    
+   
+
